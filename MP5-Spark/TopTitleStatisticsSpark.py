dbbd7e6c969b3ef4c121950335b1f406d8d6b479
Hongfei <hongfeili365@gmail.com>
2020-03-08 10:49:34 -0500

    Implement TopTitleStatisticsSpark

--- MP5-Spark/TopTitleStatisticsSpark.py
+++ MP5-Spark/TopTitleStatisticsSpark.py
@@ -8,18 +8,26 @@ sc = SparkContext(conf = conf)
 
 lines = sc.textFile(sys.argv[1],1)
 
-#TODO
+counts = lines.map(lambda line: int(line.strip().split('\t')[1].strip()))
+
+mean_ = int(counts.mean())
+sum_ = counts.sum()
+min_ = counts.min()
+max_ = counts.max()
+var_ = int(counts.variance())
+
 
 outputFile = open(sys.argv[2],"w")
 '''
-TODO write your output here
+write your output here
 write results to output file. Format
-outputFile.write('Mean\t%s\n' % ans1)
-outputFile.write('Sum\t%s\n' % ans2)
-outputFile.write('Min\t%s\n' % ans3)
-outputFile.write('Max\t%s\n' % ans4)
-outputFile.write('Var\t%s\n' % ans5)
 '''
+outputFile.write('Mean\t%s\n' % mean_)
+outputFile.write('Sum\t%s\n' % sum_)
+outputFile.write('Min\t%s\n' % min_)
+outputFile.write('Max\t%s\n' % max_)
+outputFile.write('Var\t%s\n' % var_)
 
+outputFile.close()
 sc.stop()
 
