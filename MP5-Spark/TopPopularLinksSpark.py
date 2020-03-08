174d0a0df7e7f31d693cf011285260df5c0ff041
Hongfei <hongfeili365@gmail.com>
2020-03-08 11:25:45 -0500

    Implement TopPopularLinksSpark

--- MP5-Spark/TopPopularLinksSpark.py
+++ MP5-Spark/TopPopularLinksSpark.py
@@ -2,18 +2,33 @@
 import sys
 from pyspark import SparkConf, SparkContext
 
+def link_count_mapper(line):
+    page_id, targets = line.strip().split(': ')
+    targets = targets.strip().split(' ')
+    targets = [t.strip() for t in targets]
+    targets = filter(lambda x: len(x) > 0, targets)
+
+    to_return = [(t, 1) for t in targets]
+    return to_return
+
+
 conf = SparkConf().setMaster("local").setAppName("TopPopularLinks")
 conf.set("spark.driver.bindAddress", "127.0.0.1")
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile(sys.argv[1], 1) 
 
-#TODO
+counts = lines.flatMap(link_count_mapper).reduceByKey(lambda a, b: a + b)
+
+counts.sortByKey(ascending=False)
+
+tops = counts.sortBy(ascending=False, keyfunc=lambda a: a[1]).take(10)
+tops = sorted(tops)
 
 output = open(sys.argv[2], "w")
-
-#TODO
 #write results to output file. Foramt for each line: (key + \t + value +"\n")
-
+for (page, count) in tops:
+    output.write(page+"\t"+str(count)+"\n")
+output.close()
 sc.stop()
 
