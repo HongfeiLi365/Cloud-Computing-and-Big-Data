c71bf0cc2e936084f13dc6791741f1506d513adc
Hongfei Li <hongfeili365@gmail.com>
2020-04-19 13:57:19 -0500

    Update part_d

--- MP9-SparkGraphMLLib/part_d.py
+++ MP9-SparkGraphMLLib/part_d.py
@@ -17,7 +17,19 @@ def predict(df_train, df_test):
 
     # Result: Result should be a list with the trained model's predictions
     # for all the test data points
-    return []
+    rf = RandomForestClassifier()
+    model = rf.fit(df_train)
+    pred = model.transform(df_test)
+    
+    return pred.prediction
+
+def parse_line(line):
+    # TODO: Parse data from line into an RDD
+    line = line.split(',')
+    label = line[-1] 
+    features = line[:-1] 
+    features = Vectors.dense(features)
+    return [features, label]
 
 
 def main():
@@ -27,18 +39,18 @@ def main():
     # Hint: For types and format look at what the format required by the
     # `train` method for the random forest classifier
     # Hint 2: Look at the imports above
-    rdd_train = None
+    rdd_train = raw_training_data.map(parse_line)
 
     # TODO: Create dataframe from the RDD
-    df_train = None
+    df_train = sqlContext.createDataFrame(rdd_train, ['features','label'])
 
     raw_test_data = sc.textFile("dataset/test-features.data")
 
     # TODO: Convert text file lines into an RDD we can use later
-    rdd_test = None
+    rdd_test =raw_test_data.map(lambda line: Vectors.dense(line.split(',')))
 
     # TODO:Create dataframe from RDD
-    df_test = None
+    df_test = sqlContext.createDataFrame(rdd_test, ['features'])
 
     predictions = predict(df_train, df_test)
 
