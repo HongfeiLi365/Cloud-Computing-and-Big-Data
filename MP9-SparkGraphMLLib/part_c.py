ec3522d6b6abb51b00f3b7fd7e17f41ecba0bfa9
Hongfei Li <hongfeili365@gmail.com>
2020-04-19 13:13:58 -0500

    Update part_c

--- MP9-SparkGraphMLLib/part_c.py
+++ MP9-SparkGraphMLLib/part_c.py
@@ -1,6 +1,7 @@
 from pyspark import *
 from pyspark.sql import SparkSession
 from graphframes import *
+from collections import defaultdict
 
 sc = SparkContext()
 spark = SparkSession.builder.appName('fun').getOrCreate()
@@ -11,7 +12,14 @@ def get_shortest_distances(graphframe, dst_id):
     # Find shortest distances in the given graphframe to the vertex which has id `dst_id`
     # The result is a dictionary where key is a vertex id and the corresponding value is
     # the distance of this node to vertex `dst_id`.
-    return {}
+    result = graphframe.shortestPaths(landmarks=dst_id)
+    result = result.select("id", "distances").collect()
+
+    distances = defaultdict(list)
+    for row in result:
+        distances[row['id']].append(row['distances'])
+
+    return distances
 
 
 if __name__ == "__main__":
@@ -19,15 +27,17 @@ if __name__ == "__main__":
     edge_list = []
     with open('dataset/graph.data') as f:
         for line in f:
-            # TODO: Parse line to get vertex id
-            src = None
-            # TODO: Parse line to get ids of vertices that src is connected to
-            dst_list = []
+            line = line.split()
+            src = line[0]  # TODO: Parse src from line
+            if len(line) > 1:
+                dst_list = line[1:]  # TODO: Parse dst_list from line
+            else:
+                dst_list = []
             vertex_list.append((src,))
             edge_list += [(src, dst) for dst in dst_list]
 
-    vertices = spark.createDataFrame(...)  # TODO: Create dataframe for vertices
-    edges = spark.createDataFrame(...)  # TODO: Create dataframe for edges
+    vertices = spark.createDataFrame(vertex_list, ['id'])  # TODO: Create dataframe for vertices
+    edges = spark.createDataFrame(edge_list, ['src', 'dst'])  # TODO: Create dataframe for edges
 
     g = GraphFrame(vertices, edges)
     sc.setCheckpointDir("/tmp/shortest-paths")
