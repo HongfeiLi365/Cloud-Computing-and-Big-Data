808af97f246f863bc8485a7cb3963e675734eb54
Hongfei Li <hongfeili365@gmail.com>
2020-04-19 11:08:20 -0500

    Update part_a

--- MP9-SparkGraphMLLib/part_a.py
+++ MP9-SparkGraphMLLib/part_a.py
@@ -1,6 +1,7 @@
 from pyspark import *
 from pyspark.sql import SparkSession
 from graphframes import *
+from collections import defaultdict
 
 sc = SparkContext()
 spark = SparkSession.builder.appName('fun').getOrCreate()
@@ -13,7 +14,19 @@ def get_connected_components(graphframe):
     # For example, [[a_1, a_2, ...], [b_1, b_2, ...], ...]
     # then a_1, a_2, ..., a_n lie in the same component,
     # b_1, b2, ..., b_m lie in the same component, etc
-    return [[]]
+
+    result = graphframe.connectedComponents()
+    result = result.sort("component")
+    result = result.select("id", "component").collect()
+
+    components = defaultdict(list)
+    for row in result:
+        components[row['component']].append(row['id'])
+
+    to_return = []
+    for key, value in components.items():
+        to_return.append(value)
+    return to_return
 
 
 if __name__ == "__main__":
@@ -21,13 +34,18 @@ if __name__ == "__main__":
     edge_list = []
     with open('dataset/graph.data') as f:  # Do not modify
         for line in f:
-            src = None  # TODO: Parse src from line
-            dst_list = []  # TODO: Parse dst_list from line
+            line = line.split()
+            src = line[0]  # TODO: Parse src from line
+            if len(line) > 1:
+                dst_list = line[1:]  # TODO: Parse dst_list from line
+            else:
+                dst_list = []
             vertex_list.append((src,))
             edge_list += [(src, dst) for dst in dst_list]
 
-    vertices = spark.createDataFrame(...)  # TODO: Create vertices dataframe
-    edges = spark.createDataFrame(...)  # TODO: Create edges dataframe
+    # TODO: Create vertices dataframe
+    vertices = spark.createDataFrame(vertex_list)
+    edges = spark.createDataFrame(edge_list)  # TODO: Create edges dataframe
 
     g = GraphFrame(vertices, edges)
     sc.setCheckpointDir("/tmp/connected-components")
